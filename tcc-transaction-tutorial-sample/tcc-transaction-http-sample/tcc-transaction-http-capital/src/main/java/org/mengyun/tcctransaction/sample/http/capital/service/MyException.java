7ba8e9d6326cb6c1e086797ce9363470387d1a22
changming.xie <changming.xie@192.168.0.104>
2017-04-29 16:34:04 +0800

    remove unused classes

--- tcc-transaction-tutorial-sample/tcc-transaction-http-sample/tcc-transaction-http-capital/src/main/java/org/mengyun/tcctransaction/sample/http/capital/service/MyException.java
+++
@@ -1,8 +0,0 @@
-package org.mengyun.tcctransaction.sample.http.capital.service;
-
-/**
- * Created by changming.xie on 4/26/17.
- */
-public class MyException extends RuntimeException {
-    private static final long serialVersionUID = -8179491348620839778L;
-}
