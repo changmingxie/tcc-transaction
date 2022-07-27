e28a2948a481e4ca532e60ed90004ae630e567ff
changmingxie <ytmjjje@163.com>
2022-07-27 15:16:43 +0800

    refactor client -> server connection mode, lazy connect to all server instance with load robin

--- tcc-transaction-core/src/main/java/org/mengyun/tcctransaction/remoting/RemotingClient.java
+++ tcc-transaction-core/src/main/java/org/mengyun/tcctransaction/remoting/RemotingClient.java
@@ -4,7 +4,12 @@ import org.mengyun.tcctransaction.remoting.protocol.RemotingCommand;
 
 public interface RemotingClient<T> extends RemotingService<T> {
 
-    RemotingCommand invokeSync(final String addr, final RemotingCommand request, final long timeoutMillis);
+    RemotingCommand invokeSync(final RemotingCommand request, final long timeoutMillis);
 
-    void invokeOneway(final String addr, final RemotingCommand request, final long timeoutMillis);
+    RemotingCommand invokeSync(final String address, final RemotingCommand request, final long timeoutMillis);
+
+    void invokeOneway(final RemotingCommand request, final long timeoutMillis);
+
+
+    void invokeOneway(final String address, final RemotingCommand request, final long timeoutMillis);
 }
